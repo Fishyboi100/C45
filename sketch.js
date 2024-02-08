f40664c83e2107736cf3a1293e154edf65d19730
Fishyboi100 <144295948+Fishyboi100@users.noreply.github.com>
2024-02-07 21:02:51 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+var backgroundImg,background
+var car,carImg
+var cone,coneImg
+var finishLine,finishLineImg
+
+function preload(){
+ backgroundImg = loadImage("Road.png")
+ carImg = loadImage("car2.png")
+ coneImg = loadImage("Cone.jpg")
+ finishLineImg = loadImage("Finish Line.jfif")
+}
+
+function setup(){
+ createCanvas(600,600)
+ background = createSprite(300,300)
+ background.addImage("background",backgroundImg);
+
+ finishLine.createSprite(300,100)
+ finishLine.addImage("finishLine",finishLineImg)
+
+ car = createSprite(300,550)
+ car.addImage("car",carImg)
+ car.scale = 0.05
+ 
+ 
+}
+
+function draw(){
+  if(keyIsDown(UP_ARROW)){
+    car.y = car.y - 3
+   }
+   if(keyIsDown(RIGHT_ARROW)){
+    car.x = car.x + 3
+   }
+   if(keyIsDown(LEFT_ARROW)){
+    car.x = car.x - 3
+   }
+
+  drawSprites()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
